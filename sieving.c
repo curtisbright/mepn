43fe724277cccd022a1707a9b80c2c39bab712dd
curtisbright <cbright@uwaterloo.ca>
2015-06-05 19:12:09 -0400

    make sure sieve file exists

--- sieving.c
+++ sieving.c
@@ -27,13 +27,24 @@ int main(int argc, char** argv)
 	}
 	else if(argc>1)
 	{	signal(SIGINT, sig_handler);
+
+		char filename[25];
+		sprintf(filename, "srsieve/sieve.%s.txt", argv[1]);
+		if(access(filename, F_OK)==-1)
+		{	char filename2[25], copy[100];
+			sprintf(filename2, "data/sieve.%s.txt", argv[1]);
+			if(access(filename2, F_OK)==-1)
+			{	printf("No sieve file for base %s!\n", argv[1]);
+				return 0;
+			}
+			sprintf(copy, "cp %s %s", filename2, filename);
+			system(copy);
+		}
+
 		for(;;)
 		{	pid = fork();
 			if(pid==0)
-			{	char filename[25];
-				sprintf(filename, "srsieve/sieve.%s.txt", argv[1]);
 				execlp("timeout", "timeout", "120m", "srsieve/srsieve", filename, "-o", filename, NULL);
-			}
 			int status;
 			wait(&status);
 			system("./merge");
